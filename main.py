5a26543d95ee3bbe060c3c848d588ebcf709061b
Denis <onlyname@mail.ru>
2023-06-29 16:35:28 +0300

    download books with titles.

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import requests
 import os
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
 
-def download_books():
+def get_book_title(number):
+    url = f'https://tululu.org/b{number}/'
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response.history)
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find('head').find('title')
+    book_title, author = title_tag.text.split(' - ')
+    book_name = sanitize_filename(book_title)
+    book_author = author.split(',')[0].strip()
+    return book_name, book_author
+
+
+def download_txt(number, filename, folder='books/'):
     os.makedirs("books", exist_ok=True)
+    url = f"https://tululu.org/txt.php?id={number}"
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response.history)
+    with open(os.path.join(folder, f'{number}.{filename}.txt'), 'wb') as file:
+        file.write(response.content)
+
+
+def check_for_redirect(response_history):
+    if response_history:
+        raise requests.HTTPError
+
+
+def main():
     for number in range(1, 11, 1):
-        url = f"https://tululu.org/txt.php?id={number}"
-        response = requests.get(url)
-        response.raise_for_status()
-        filename = f"book_{number}.txt"
-        with open(f"books/{filename}", 'wb') as file:
-            file.write(response.content)
+        try:
+            filename, author = get_book_title(number)
+            download_txt(number, filename, folder='books/')
+        except requests.HTTPError:
+            print(f"Книги {number} нет в каталоге")
 
 
 if __name__ == "__main__":
-    download_books()
+    main()
